86ea871222baadacbeedeaccb312cefd6f0887c0
Ashmit Raj <trivediashmit3@gmail.com>
2026-09-03 13:12:10 +0530

    Ignore Next.js build cache

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ydy6AYRyOLiFvdAmXQ5GroNo6JuZuzrcOZFrqK5aFn8=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_177wwqa._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_1f-k72q._.js","static/chunks/node_modules_next_0rxjrz0._.js","static/chunks/node_modules_motion-dom_dist_es_0g6cuzy._.js","static/chunks/node_modules_framer-motion_dist_es_1jg1qf3._.js","static/chunks/node_modules_0o0kzch._.js"],
-    source: "dynamic"
-});
